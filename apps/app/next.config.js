33c7833944ed915910c6c96b4018a2f32d7701df
Michal Jalowik <michaljalowik@MacBook-Pro-Michal.local>
2023-11-24 23:12:22 +0100

    Circulat 3

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -11,21 +11,19 @@ const nextConfig = {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
-  },
-  async rewrites() {
-		    return [
-      {
-        source: '/api',
-        // destination: `https://${process.env.API_URL}/api` || `https://nx-boila-awesome.vercel.app/api` || `http://localhost:3000/api`,
-        destination: `https://nx-boila-awesome.vercel.app/api`,
-      }
-      // {
-      //   source: '/api/:path*',
-      //   // destination: `https://${process.env.API_URL}/api/:path*` || `https://nx-boila-awesome.vercel.app/api/:path*` || `http://localhost:3000/api/:path*`,
-      //   destination: `https://nx-boila-awesome.vercel.app/api/:path*`,
-      // },
-    ];
-	},
+  }
+  // async rewrites() {
+	// 	    return [
+  //     {
+  //       source: '/api',
+  //       destination: `http://${process.env.API_URL}/api` || `http://localhost:3000/api`,
+  //     },
+  //     {
+  //       source: '/api/:path*',
+  //       destination: `http://${process.env.API_URL}/api/:path*` || `http://localhost:3000/api/:path*`,
+  //     },
+  //   ];
+	// },
 };
 
 module.exports = withNx(nextConfig);
